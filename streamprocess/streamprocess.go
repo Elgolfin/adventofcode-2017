d5b24c3662d2f6c71347555bca4e016a6c2d8251
Elgolfin <elgo@nicomputer.net>
2017-12-09 10:52:09 -0500

    Add comments to document the Score function
    About how the garbage size is handled in the function.

--- streamprocess/streamprocess.go
+++ streamprocess/streamprocess.go
@@ -1,6 +1,6 @@
 package streamprocess
 
-// Score returns the score and the number of characters  within the garbage of the content string based on the rules of the Day 9
+// Score returns the score and the number of characters within the garbage of the content string based on the rules of the Day 9
 func Score(content string) (int, int) {
 	score := 0
 	currentGroupLevel := 0
@@ -10,6 +10,7 @@ func Score(content string) (int, int) {
 	for _, r := range content {
 		c := string(r)
 
+		// Increment the garbage if we are in the garbage stream, we will adjust the size of the special cases afterwards
 		if isGarbage {
 			garbageSize++
 		}
@@ -17,6 +18,7 @@ func Score(content string) (int, int) {
 		// If in a garbage stream, ignore the character following a !
 		if isGarbage && previousChar == "!" {
 			previousChar = ""
+			// The escape character and the escaped character do not count towards the garbage size, decrement the current size by 2
 			garbageSize -= 2
 			continue
 		}
@@ -31,6 +33,7 @@ func Score(content string) (int, int) {
 			isGarbage = true
 		case isGarbage && c == ">":
 			isGarbage = false
+			// The character ending the garbage does not count towards the garbage size, decrement the current size by 1
 			garbageSize--
 		}
 
