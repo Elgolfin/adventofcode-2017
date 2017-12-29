02c1bdc6e85bf0cd6d13616dc0a3ff242a28d9ea
Elgolfin <elgo@nicomputer.net>
2017-12-29 09:56:23 -0500

    Add a link to a stackoverflow post about permutation

--- helper/helper.go
+++ helper/helper.go
@@ -28,3 +28,39 @@ func Permut(arr []int) [][]int {
 	helper(len(arr), arr)
 	return res
 }
+
+/*
+https://stackoverflow.com/questions/30226438/generate-all-permutations-in-go
+
+Here's code that iterates over all permutations without generating them all first. The slice p keeps the intermediate state as offsets in a Fisher-Yates shuffle algorithm. This has the nice property that the zero value for p describes the identity permutation.
+
+package main
+
+import "fmt"
+
+func nextPerm(p []int) {
+    for i := len(p) - 1; i >= 0; i-- {
+        if i == 0 || p[i] < len(p)-i-1 {
+            p[i]++
+            return
+        }
+        p[i] = 0
+    }
+}
+
+func getPerm(orig, p []int) []int {
+    result := append([]int{}, orig...)
+    for i, v := range p {
+        result[i], result[i+v] = result[i+v], result[i]
+    }
+    return result
+}
+
+func main() {
+    orig := []int{11, 22, 33}
+    for p := make([]int, len(orig)); p[0] < len(p); nextPerm(p) {
+        fmt.Println(getPerm(orig, p))
+    }
+}
+
+*/
